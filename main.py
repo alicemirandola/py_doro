08a983791e3583946a33b738f1d7b62186bf9101
Marco Valente <io.marco.valente@gmail.com>
2019-12-02 00:24:59 +0000

    Update main.py (#2)

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import pandas as pd
 
-def main():
 
+def main():
     xls = pd.ExcelFile("sample_spreadsheet.xlsx")
 
-    sheetX = xls.parse(0) #2 is the sheet number
+    sheetX = xls.parse(0)  # 2 is the sheet number
 
     var1 = sheetX['Year']
 
-    print(var1[2]) #1 is the row number...
+    print(var1[2])  # 1 is the row number...
+
 
 main()
